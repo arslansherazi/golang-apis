abd1097a0ee3c9672344257e21c9d164a4d58761
arslansherazi <arslanhaider.dev@hotmail.com>
2022-03-31 17:35:11 +0500

    create json data

--- json/create_json_data.go
+++ json/create_json_data.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+)
 
+type Course struct {
+	Name     string
+	Price    int
+	Platform string
+	Tags     []string
+	Ratings  float64
+}
+
+func main() {
+	courses := []Course{
+		{"Artificial Intelligence", 1950, "Udemy", []string{"ai", "a-z", "udemy"}, 4.2},
+		{"Go Lang", 3500, "Coursera", []string{"go-lang", "speed", "udemy"}, 4.2},
+	}
+
+	finalJson, error := json.MarshalIndent(courses, "", "\t")
+	if error != nil {
+		panic(error)
+	}
+	fmt.Printf("Type of json data:: %T", finalJson)
+	fmt.Println()
+	fmt.Printf("%s\n", finalJson)
 }
